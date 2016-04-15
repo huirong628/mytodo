631b32c165c8f0b6116e8290c7fc18034765b4e9
ZhangHuirong <mrzhang628@gmail.com>
2016-04-15 19:29:01 +0800

    Reference the RecycleView.

---
+++ app/src/main/java/com/android/huirongzhang/todo/pulltorefresh/RecyclerViewAdapter.java
@@ -0,0 +1,25 @@
+package com.android.huirongzhang.todo.pulltorefresh;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by zhanghuirong on 2016/4/15.
+ */
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
