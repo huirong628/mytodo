027fbbc2b9d74d9ce2f7e100a141cb8fb57b7515
ZhangHuirong <mrzhang628@gmail.com>
2016-04-20 11:37:08 +0800

    Update the folder item ui and some icons.

--- app/src/main/java/com/android/huirongzhang/todo/folder/FolderAdapter.java
+++ app/src/main/java/com/android/huirongzhang/todo/folder/FolderAdapter.java
@@ -18,69 +18,71 @@ import java.util.List;
  */
 public class FolderAdapter extends BaseAdapter {
 
-    private List<Folder> mFolders;
+	private List<Folder> mFolders;
 
-    private FolderItemListener mItemListener;
+	private FolderItemListener mItemListener;
 
-    public FolderAdapter(ArrayList<Folder> folders, FolderItemListener itemListener) {
-        setList(folders);
-        mItemListener = itemListener;
-    }
+	public FolderAdapter(ArrayList<Folder> folders, FolderItemListener itemListener) {
+		setList(folders);
+		mItemListener = itemListener;
+	}
 
-    public void replaceData(List<Folder> folders) {
-        setList(folders);
-        notifyDataSetChanged();
-    }
+	public void replaceData(List<Folder> folders) {
+		setList(folders);
+		notifyDataSetChanged();
+	}
 
-    private void setList(List<Folder> folders) {
-        mFolders = folders;
-    }
+	private void setList(List<Folder> folders) {
+		mFolders = folders;
+	}
 
-    @Override
-    public int getCount() {
-        return mFolders.size();
-    }
+	@Override
+	public int getCount() {
+		return mFolders.size();
+	}
 
-    @Override
-    public Object getItem(int i) {
-        return mFolders.get(i);
-    }
+	@Override
+	public Object getItem(int i) {
+		return mFolders.get(i);
+	}
 
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
+	@Override
+	public long getItemId(int i) {
+		return i;
+	}
 
-    @Override
-    public View getView(int i, View convert, ViewGroup viewGroup) {
+	@Override
+	public View getView(int i, View convert, ViewGroup viewGroup) {
 
-        View view = convert;
-        ViewHolder viewHolder;
-        if (view == null) {
-            viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
-            view = inflater.inflate(R.layout.folder_item, viewGroup, false);
-            viewHolder.name = (TextView) view.findViewById(R.id.folder_name);
-            view.setTag(viewHolder);
+		View view = convert;
+		ViewHolder viewHolder;
+		if (view == null) {
+			viewHolder = new ViewHolder();
+			LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+			view = inflater.inflate(R.layout.folder_item, viewGroup, false);
+			viewHolder.name = (TextView) view.findViewById(R.id.folder_name);
+			viewHolder.num = (TextView) view.findViewById(R.id.task_num);
+			view.setTag(viewHolder);
 
-        } else {
-            viewHolder = (ViewHolder) view.getTag();
-        }
+		} else {
+			viewHolder = (ViewHolder) view.getTag();
+		}
 
-        final Folder folder = (Folder) getItem(i);
+		final Folder folder = (Folder) getItem(i);
 
-        viewHolder.name.setText(folder.getTitle());
+		viewHolder.name.setText(folder.getTitle());
 
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mItemListener.onFolderClick(folder);
-            }
-        });
-        return view;
-    }
+		view.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				mItemListener.onFolderClick(folder);
+			}
+		});
+		return view;
+	}
 
-    private static class ViewHolder {
-        TextView name;
-    }
+	private static class ViewHolder {
+		TextView name;
+		TextView num;
+	}
 }
