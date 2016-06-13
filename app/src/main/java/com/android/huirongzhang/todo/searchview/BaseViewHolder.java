2395ff7609ccfabb46415d91e0da309255f13b3f
ZhangHuirong <mrzhang628@gmail.com>
2016-06-13 17:49:27 +0800

    RecycleView使用优化，添加必要的注释

--- app/src/main/java/com/android/huirongzhang/todo/searchview/BaseViewHolder.java
+++ app/src/main/java/com/android/huirongzhang/todo/searchview/BaseViewHolder.java
@@ -17,8 +17,22 @@ public abstract class BaseViewHolder<V> extends RecyclerView.ViewHolder {
         super(LayoutInflater.from(context).inflate(layoutRes, root, false));
     }
 
+    /**
+     * 抽象方法，绑定数据.
+     * 让子类自行对数据和view进行绑定
+     *
+     * @param itemValue Item的数据
+     * @param position  当前item的position
+     */
     protected abstract void bindData(V itemValue, int position);
 
+    /**
+     * 用于传递数据和信息
+     *
+     * @param itemValue
+     * @param position
+     *
+     */
     public void setData(V itemValue, int position) {
         Log.d(TAG, "setData()");
         bindData(itemValue, position);
