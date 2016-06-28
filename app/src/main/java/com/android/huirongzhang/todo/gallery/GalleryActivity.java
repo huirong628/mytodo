5284d5c819b72d3cbc6f42447ec0323eb8d8edc6
ZhangHuirong <mrzhang628@gmail.com>
2016-06-28 15:20:50 +0800

    增加下拉刷新上拉加载更多功能

---
+++ app/src/main/java/com/android/huirongzhang/todo/gallery/GalleryActivity.java
@@ -0,0 +1,16 @@
+package com.android.huirongzhang.todo.gallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhanghuirong on 2016/6/17.
+ */
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/android/huirongzhang/todo/test/PullToRefreshTest.java
@@ -0,0 +1,17 @@
+package com.android.huirongzhang.todo.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.huirongzhang.todo.R;
+
+/**
+ * Created by zhanghuirong on 2016/6/28.
+ */
+public class PullToRefreshTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_pull_to_refresh_test);
+    }
+}
