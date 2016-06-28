ba4b7b576077baddaa8db647768276b9845ab2ad
HuirongZhang <mrzhang628@gmail.com>
2016-06-28 20:07:57 +0800

    增加下拉刷新上拉加载更多功能 (cherry picked from commit 341374e)

--- app/src/main/java/com/android/huirongzhang/todo/test/PullToRefreshTest.java
+++ app/src/main/java/com/android/huirongzhang/todo/test/PullToRefreshTest.java
@@ -1,17 +1,56 @@
 package com.android.huirongzhang.todo.test;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.android.huirongzhang.todo.R;
+import com.android.huirongzhang.todo.pulltorefresh.PullToRefreshBase;
+import com.android.huirongzhang.todo.pulltorefresh.PullToRefreshListView;
 
 /**
  * Created by zhanghuirong on 2016/6/28.
  */
 public class PullToRefreshTest extends Activity {
+    private PullToRefreshListView mPullToRefresh;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_pull_to_refresh_test);
+        mPullToRefresh = (PullToRefreshListView) findViewById(R.id.pull_to_refresh);
+        mPullToRefresh.setMode(PullToRefreshBase.Mode.BOTH);
+        mPullToRefresh.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+                new GetDataTask().execute();
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+                new GetDataTask().execute();
+            }
+        });
+    }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // Simulates a background job.
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            mPullToRefresh.onRefreshComplete();
+
+            super.onPostExecute(result);
+        }
     }
 }
