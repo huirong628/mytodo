54a70742fb62d0b4d2b0718a50186d6d36f70ca9
HuirongZhang <mrzhang628@gmail.com>
2016-09-29 14:04:48 +0800

    【WebView】Update WebView

--- app/src/main/java/com/android/huirongzhang/todo/test/WebViewTest.java
+++ app/src/main/java/com/android/huirongzhang/todo/test/WebViewTest.java
@@ -4,12 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.android.huirongzhang.todo.R;
 import com.android.huirongzhang.todo.webview.WebAppInterface;
@@ -22,9 +27,11 @@ public class WebViewTest extends Activity {
 
     private static final String WEB_PAGE = "http://fa.163.com/activity/silver/newIndex/wap/sign.do?from=pz360et1";
     private static final String WEB_LIVE = "http://h.huajiao.com/l/index?liveid=sn._LC_RE_non_2497993014745280621098581_SX&author=24979930&userid=24979930&version=4.2.1.1019&time=1474528092&reference=wx&qd=wx&channel=meizu";
+    private static final String TAG = "WebViewTest";
 
     private WebView mWebView;
     private View mLoadingView;
+    private TextView mLoadingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,8 @@ public class WebViewTest extends Activity {
 
         mLoadingView = findViewById(R.id.loading_view);
 
+        mLoadingText = (TextView) findViewById(R.id.loading_text);
+
         initWebView();
 
         initWebSettings();
@@ -40,7 +49,7 @@ public class WebViewTest extends Activity {
 
     private void initWebView() {
         mWebView = (WebView) findViewById(R.id.web_view);
-        mWebView.loadUrl(WEB_PAGE);
+
         /**
          * Binding JavaScript code to Android code
          */
@@ -59,6 +68,10 @@ public class WebViewTest extends Activity {
         mWebView.setWebViewClient(new WebViewClient());
 
         /**
+         * To handle navigation inside the WebView
+         *
+         * You can use it to control how the WebView handles link clicks and page redirects.
+         *
          * 自定义WebViewClient,控制会更加灵活。
          *
          */
@@ -68,7 +81,9 @@ public class WebViewTest extends Activity {
         /**
          *
          */
-        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebChromeClient(new MyWebChromeClient());
+
+        mWebView.loadUrl(WEB_LIVE);
     }
 
     private void initWebSettings() {
@@ -83,6 +98,8 @@ public class WebViewTest extends Activity {
          *
          */
         webSettings.setJavaScriptEnabled(true);//enable js
+
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
 
         /**
          * ua 用处：
@@ -100,7 +117,7 @@ public class WebViewTest extends Activity {
          */
         webSettings.setUserAgentString(null);
 
-        //
+        //缓存机制
         webSettings.setDomStorageEnabled(true);
 
         /**
@@ -112,6 +129,31 @@ public class WebViewTest extends Activity {
         //webSettings.setSaveFormData(true);
 
 
+        /**
+         * 优化部分
+         *
+         * Web先不要自动加载图片，等页面finish之后再发起图片加载。
+         *
+         * 对系统API在19以上的版本作了兼容。
+         *
+         * 因为4.4以上系统在onPageFinished时再恢复图片加载时,
+         *
+         * 如果存在多张图片引用的是相同的src时，
+         *
+         * 会只有一个image标签得到加载，
+         *
+         * 因而对于这样的系统我们就先直接加载。
+         */
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            webSettings.setLoadsImagesAutomatically(true);
+        } else {
+            webSettings.setLoadsImagesAutomatically(false);
+        }
+
+        //显示缩放按钮
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
     }
 
     /**
@@ -131,6 +173,7 @@ public class WebViewTest extends Activity {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.d(TAG, "shouldOverrideUrlLoading()");
 
             /**
              * return false :let my WebView load the page.
@@ -148,22 +191,78 @@ public class WebViewTest extends Activity {
              * return true:启动其它程序处理urls
              */
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
             return true;
         }
 
         @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+            return super.shouldInterceptRequest(view, request);
+        }
+
+        @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            Log.d(TAG, "onPageStarted()");
             super.onPageStarted(view, url, favicon);
             mLoadingView.setVisibility(View.VISIBLE);
-            mWebView.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Log.d(TAG, "onReceivedError()");
+            // super.onReceivedError(view, errorCode, description, failingUrl);
+            /**
+             * 清除默认错误页内容
+             */
+            // view.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            /**
+             *  设置定义view
+             */
+            mLoadingText.setText("网络出问题了...");
         }
 
         @Override
         public void onPageFinished(WebView view, String url) {
+            Log.d(TAG, "onPageFinished()");
             super.onPageFinished(view, url);
+
             mLoadingView.setVisibility(View.GONE);
-            mWebView.setVisibility(View.VISIBLE);
+
+            if (!mWebView.getSettings().getLoadsImagesAutomatically()) {
+                mWebView.getSettings().setLoadsImagesAutomatically(true);
+            }
+        }
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+        }
+
+        /**
+         * In order to support full screen — for video or other HTML content
+         *
+         * @param view
+         * @param callback
+         */
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            super.onShowCustomView(view, callback);
+        }
+
+        @Override
+        public void onHideCustomView() {
+            super.onHideCustomView();
+        }
+
+        @Override
+        public View getVideoLoadingProgressView() {
+            return super.getVideoLoadingProgressView();
         }
     }
 
