92c7b9d299d5293108bc945b21092c347ac26863
HuirongZhang <mrzhang628@gmail.com>
2016-10-15 10:18:35 +0800

    【自定义View】绘制风扇的白色和黄色背景

--- app/src/main/java/com/android/huirongzhang/todo/widget/LoadingView.java
+++ app/src/main/java/com/android/huirongzhang/todo/widget/LoadingView.java
@@ -21,6 +21,9 @@ import static android.view.View.MeasureSpec.AT_MOST;
 public class LoadingView extends View {
     private static final String TAG = "LoadingView";
     private static final int DEFAULT_BG_OUTER = 0xfffde399; // 外部边框的背景颜色
+    private static final String DEFAULT_WHITE = "#fffefd";
+    private static final String DEFAULT_BG_FAN = "#fcce5b";  // 风扇 扇叶的颜色
+
     private static final int DEFAULT_WIDTH = 300;
     private static final int DEFAULT_HEIGHT = 600;
 
@@ -33,6 +36,9 @@ public class LoadingView extends View {
     //自定义路径
     private RectF outerCircle;
     private RectF outerRectangle;
+    private RectF innerCircle;
+    private RectF innerRectangle;
+    private RectF fanWhiteRect;
 
     //外部圆半径 内部圆半径  风扇背景的半径
     private float outerRadius;
@@ -41,6 +47,8 @@ public class LoadingView extends View {
 
     //自定义画笔
     private Paint outerPaint;
+    private Paint fanPaint;
+    private Paint fanBgPaint;
 
     public LoadingView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -49,9 +57,16 @@ public class LoadingView extends View {
 
     private void init() {
         outerPaint = new Paint();
-        outerPaint.setColor(Color.RED);
-        outerPaint.setStyle(Paint.Style.STROKE);
-        outerPaint.setStrokeWidth(5);
+        outerPaint.setColor(DEFAULT_BG_OUTER);
+        outerPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        fanPaint = new Paint();
+        fanPaint.setAntiAlias(true);
+        fanPaint.setStyle(Paint.Style.FILL);
+        fanPaint.setColor(Color.parseColor(DEFAULT_WHITE));
+
+        fanBgPaint = new Paint(fanPaint);
+        fanBgPaint.setColor(Color.parseColor(DEFAULT_BG_FAN));
     }
 
     /**
@@ -85,9 +100,13 @@ public class LoadingView extends View {
         mWidth = w;
         mHeight = h;
         outerRadius = ((w / 10) < (h / 2)) ? w / 10 : h / 2;
+        innerRadius = outerRadius * 0.8f;
         Log.d(TAG, "onSizeChanged(),mWidth = " + mWidth + ",mHeight =" + mHeight + ",outerRadius = " + outerRadius);
-        outerRectangle = new RectF(0, 0, 8 * outerRadius, outerRadius * 2);
-        outerCircle = new RectF(-outerRadius, 0, outerRadius, outerRadius * 2);
+        outerRectangle = new RectF(outerRadius, 0, 7 * outerRadius, outerRadius * 2);
+        outerCircle = new RectF(0, 0, outerRadius * 2, outerRadius * 2);
+
+        fanWhiteRect = new RectF(7 * outerRadius, outerRadius, 9 * outerRadius, outerRadius * 3);
+        fanBgRadius = outerRadius * 0.8f;
     }
 
     /**
@@ -106,6 +125,17 @@ public class LoadingView extends View {
             outBorderBitmapCache = new WeakReference<Bitmap>(outBorderBitmap);
         }
         canvas.drawBitmap(outBorderBitmap, 0, 0, outerPaint);
+
+        drawInnerCircle(canvas);
+
+        //风扇的白色背景
+        canvas.drawArc(fanWhiteRect, 90, 360, true, fanPaint);
+
+        //风扇的黄色背景
+        canvas.save();
+        canvas.scale(0.9f, 0.9f, 8 * outerRadius, 2 * outerRadius);
+        canvas.drawArc(fanWhiteRect, 90, 360, true, fanBgPaint);
+        canvas.restore();
     }
 
     //进度条外面的背景
@@ -113,9 +143,14 @@ public class LoadingView extends View {
         //java.lang.IllegalArgumentException: width and height must be > 0
         Bitmap bitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);//根据指定参数生成新位图，图片压缩质量参数。
         Canvas canvas = new Canvas(bitmap);
-        canvas.translate(mWidth / 10, mHeight / 4);
-        canvas.drawArc(outerCircle, 90, 180, true, outerPaint);
+        canvas.translate(mWidth / 10, mWidth / 10);
         canvas.drawRect(outerRectangle, outerPaint);
+        canvas.drawArc(outerCircle, 90, 180, true, outerPaint);
+
         return bitmap;
+    }
+
+    private void drawInnerCircle(Canvas canvas) {
+
     }
 }
